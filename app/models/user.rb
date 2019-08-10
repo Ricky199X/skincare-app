699af6e3d62d6770bd94c10dcedbc23640c4b1a9
github username <github email address>
2019-08-09 21:51:23 -0400

    adds slug to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
    has_many :routines
+
+   #validate user name, password, and email address
+   validates :username, :password, :email, presence: true
+   
+   #validate uniqueness of username
+   validates :username, uniqueness: true
+
+   def slug
+      username.downcase.gsub(" ", "-")
+   end
+  
+   def self.find_by_slug(slug)
+      self.all.find{|user| user.slug == slug}
+   end
 end
