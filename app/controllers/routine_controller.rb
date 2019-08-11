9633ed3e81877fffd1c7457a33c97ba02c88321c
github username <github email address>
2019-08-11 16:11:08 -0400

    adds new routine to user routines

--- app/controllers/routine_controller.rb
+++ app/controllers/routine_controller.rb
@@ -11,4 +11,17 @@ class RoutineController < ApplicationController
       erb :'routines/new'
    end
 
+   post '/routines' do
+      u = current_user
+      u.routines.create(name: params[:name], description: params[:description])
+         # binding.pry   
+      # if routine is saved properly, go to the user's routines page
+      if !!u 
+         redirect '/routines' 
+      else
+         @message = "You made an oops! Add a new routine!"
+         erb :'routines/new'
+      end
+   end
+
 end
