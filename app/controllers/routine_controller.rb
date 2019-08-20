04813f46e6e8a5da1585bb34159b158ee8f9434b
github username <github email address>
2019-08-19 22:32:15 -0400

    updates routines post, protects from empty strings

--- app/controllers/routine_controller.rb
+++ app/controllers/routine_controller.rb
@@ -12,18 +12,24 @@ class RoutineController < ApplicationController
    end
 
    # allows user to create a new routine via the UI, persists it to the database + adds to the user's routine index
+   # protects from blank values being persisted to database
+
    post '/routines' do
-      u = current_user
-      u.routines.create(name: params[:name], description: params[:description])
-         # binding.pry   
-      # if routine is saved properly, go to the user's routines page
-      if !!u 
-         redirect '/routines' 
+      if logged_in?
+        if params[:name] == "" || params[:description] == ""
+          redirect to "/routines/new"
+        else
+          @routine = current_user.routines.build(name: params[:name], description: params[:description])
+          if @routine.save
+            redirect to "/routines/#{@routine.id}"
+          else
+            redirect to "/routines/new"
+          end
+        end
       else
-         @message = "You made an oops! Add a new routine!"
-         erb :'routines/new'
+        redirect to '/login'
       end
-   end
+    end
 
    
 
